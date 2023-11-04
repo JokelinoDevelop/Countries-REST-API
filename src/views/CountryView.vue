7075e56891fcc7507afd45aa0555a90a31f91349
JokelinoDevelop <veljanoski.jovan100@gmail.com>
2023-11-04 13:19:15 +0100

    little updates

--- src/views/CountryView.vue
+++ src/views/CountryView.vue
@@ -1,81 +1,98 @@
 <template>
-  <div class="bg-light-background px-8 py-8 h-full">
+  <div class="bg-light-background px-8 py-8 h-full lg:py-12 lg:px-24">
     <router-link :to="{ path: '/' }">
       <button class="shadow-lg bg-white py-1 px-5 mb-12">
         <i class="fa-solid fa-arrow-left pr-3"></i>Back
       </button>
     </router-link>
 
-    <div v-if="store.selectedCountry" class="bg-light-background py-8">
-      <img :src="store.selectedCountry.flag" alt="" class="w-full h-60 object-cover" />
-      <div class="flex flex-col gap-2 mt-10">
-        <h1 class="font-[800] text-xl mb-4">{{ store.selectedCountry.name }}</h1>
-        <p><span class="font-[600]">Native Name:</span> {{ store.selectedCountry.nativeName }}</p>
-        <p><span class="font-[600]">Population:</span> {{ store.selectedCountry.population }}</p>
-        <p><span class="font-[600]">Region:</span> {{ store.selectedCountry.region }}</p>
-        <p><span class="font-[600]">Sub Region:</span> {{ store.selectedCountry.subregion }}</p>
-        <p><span class="font-[600]">Capital:</span> {{ store.selectedCountry.capital }}</p>
+    <div v-if="store.selectedCountry" class="bg-light-background py-8 lg:grid lg:grid-cols-6 lg:items-center lg:gap-12">
+      <div class="col-start-1 col-end-4">
+        <img :src="store.selectedCountry.flag" alt="" class="w-full h-[15rem] md:h-[20rem] lg:h-[25rem] object-cover" />
       </div>
 
-      <div class="flex flex-row gap-2 mt-10">
-        <p><span class="font-[600]">Top Level Domain:</span></p>
-        <template v-for="(domain, index) in store.selectedCountry.topLevelDomain" :key="index">
-          <p>{{ domain }}</p>
-          <span v-show="index != store.selectedCountry.topLevelDomain.length - 1">,</span>
-        </template>
+      <div class="col-start-4 col-end-7">
+        <h1 class="font-[800] text-xl my-4">{{ store.selectedCountry.name }}</h1>
+
+        <div class="lg:flex lg:gap-14">
+          <div class="flex flex-col gap-2 mt-4">
+            <p><span class="font-[600]">Native Name:</span> {{ store.selectedCountry.nativeName }}</p>
+            <p><span class="font-[600]">Population:</span> {{ store.selectedCountry.population }}</p>
+            <p><span class="font-[600]">Region:</span> {{ store.selectedCountry.region }}</p>
+            <p><span class="font-[600]">Sub Region:</span> {{ store.selectedCountry.subregion }}</p>
+            <p><span class="font-[600]">Capital:</span> {{ store.selectedCountry.capital }}</p>
+          </div>
+
+          <div>
+            <div class="flex flex-row gap-2 mt-10 lg:mt-4">
+              <p><span class="font-[600]">Top Level Domain:</span></p>
+              <template v-for="(domain, index) in store.selectedCountry.topLevelDomain" :key="index">
+                <p>{{ domain }}</p>
+                <span v-show="index != store.selectedCountry.topLevelDomain.length - 1">,</span>
+              </template>
+            </div>
+
+            <div class="flex flex-row gap-2">
+              <p><span class="font-[600]">Currencies:</span></p>
+              <template v-for="(currency, index) in store.selectedCountry.currencies" :key="index">
+                <p>{{ currency.name }}</p>
+                <span v-show="index != store.selectedCountry.currencies.length - 1">,</span>
+              </template>
+            </div>
+
+            <div class="flex flex-row gap-2">
+              <p><span class="font-[600]">Languages:</span></p>
+              <template v-for="(language, index) in store.selectedCountry.languages" :key="index">
+                <p>
+                  {{ language.name
+                  }}<span v-show="index != store.selectedCountry.languages.length - 1">,</span>
+                </p>
+              </template>
+            </div>
+          </div>
+
+
+        </div>
+
+
+        <div class="lg:grid lg:grid-cols-4 lg:grid-rows-2 lg:mt-8">
+          <p class="mt-4 lg:col-start-1 lg:col-end-2"><span class="font-[600] text-lg">Border Countries:</span></p>
+          <ul class="flex flex-row flex-wrap gap-3 mt-4 lg:col-start-2 lg:col-end-5">
+            <li class="shadow-lg bg-white py-1 px-5" v-for="(border, index) in store.selectedCountry.borders"
+              :key="index">
+              <router-link
+                :to="{ params: { alpha3Code: getBorderCountryDetails(border).alpha3Code }, name: 'CountryView' }">{{
+                  getBorderCountryDetails(border).name }}</router-link>
+            </li>
+          </ul>
+        </div>
+
       </div>
 
-      <div class="flex flex-row gap-2">
-        <p><span class="font-[600]">Currencies:</span></p>
-        <template v-for="(currency, index) in store.selectedCountry.currencies" :key="index">
-          <p>{{ currency.name }}</p>
-          <span v-show="index != store.selectedCountry.currencies.length - 1">,</span>
-        </template>
-      </div>
 
-      <div class="flex flex-row gap-2">
-        <p><span class="font-[600]">Languages:</span></p>
-        <template v-for="(language, index) in store.selectedCountry.languages" :key="index">
-          <p>
-            {{ language.name
-            }}<span v-show="index != store.selectedCountry.languages.length - 1">,</span>
-          </p>
-        </template>
-      </div>
-
-      <p class="mt-4"><span class="font-[600] text-lg">Border Countries:</span></p>
-      <ul class="flex flex-row gap-3 mt-4">
-        <li
-          class="shadow-lg bg-white py-1 px-5"
-          v-for="(border, index) in store.selectedCountry.borders"
-          :key="index"
-        >
-        <router-link :to="{params: {alpha3Code:getBorderCountryDetails(border).alpha3Code}, name: 'CountryView'}">{{ getBorderCountryDetails(border).name }}</router-link>
-        </li>
-      </ul>
 
     </div>
   </div>
 </template>
 
 <script setup>
-import {useCountryStore} from '@/stores/countryStore'
-import {onBeforeMount, onBeforeUpdate} from "vue";
-import {useRoute} from "vue-router";
+import { useCountryStore } from '@/stores/countryStore'
+import { onBeforeMount, onBeforeUpdate } from "vue";
+import { useRoute } from "vue-router";
 
 const store = useCountryStore()
 const route = useRoute()
 
-onBeforeMount(async ()=>{
+onBeforeMount(async () => {
   await store.fetchCountries()
   store.selectCountryByRoute(route.params.alpha3Code)
 })
 
-onBeforeUpdate(()=>{
+onBeforeUpdate(() => {
   store.selectCountryByRoute(route.params.alpha3Code)
 })
 
-function getBorderCountryDetails(borderAlphaCode){
+function getBorderCountryDetails(borderAlphaCode) {
   return store.countries.find((country) => country.alpha3Code === borderAlphaCode);
 }
 </script>
